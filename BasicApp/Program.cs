eb8f88733fb799c13f34986b04bdfa6eeefea928
Saroj chy <sarojdangaura321@gmail.com>
2024-09-04 08:17:24 +0545

    Data type commits

--- BasicApp/Program.cs
+++ BasicApp/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Datatypes
+//numbers
+//integers
+byte personAge = 28;
+short noOfStudents = 1800;
+int population = 2600277;
+long wieghtOfEarth = 76006656676566;
+
+//floating point number: pricision (Accuracy)
+float weight = 3442.763673637637f;
+double weight1 = 6553.776524242434353;
+decimal weight2 = 76.76635242442439903837262m;
+
+//text
+string name = "Saroj";
+char gender = 'M';
+
+//boolean
+bool isAbsent = true;
+
+//custom
+DateTime dob = DateTime.Now;
+
+
+var fullname = 89       //type inference
+int? marks = null;      //all types
+marks = 43;
